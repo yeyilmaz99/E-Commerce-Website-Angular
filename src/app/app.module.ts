661ba6eaef1913ec4aaa42223a82ae2068965d10
Yeyilmaz99 <yeyilmaz34@gmail.com>
2021-12-09 04:37:00 +0300

    stepperModifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { HttpClientModule } from '@angular/common/http';
 import { MatStepperModule } from '@angular/material/stepper';
-import { MatFormField } from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';;
 
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
@@ -18,9 +23,9 @@ import { ProductsComponent } from './products/products.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DetailComponent } from './detail/detail.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { MatNativeDateModule } from '@angular/material/core';
 import { CartComponent } from './cart/cart.component';
 import { StepperComponent } from './stepper/stepper.component';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -33,20 +38,25 @@ import { StepperComponent } from './stepper/stepper.component';
     ProductsComponent,
     DetailComponent,
     CartComponent,
-    StepperComponent
+    StepperComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MatStepperModule,
+    MatFormFieldModule,
     FormsModule,
     ReactiveFormsModule,
-    MatFormField,
-    MatNativeDateModule,
     AppRoutingModule,
     MatPaginatorModule,
-    MatStepperModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -6,19 +6,37 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   templateUrl: './stepper.component.html',
   styleUrls: ['./stepper.component.css']
 })
-export class StepperComponent implements OnInit  {
+export class StepperComponent implements OnInit   {
   isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
-
+  thirdFormGroup: FormGroup;
+  fourtFormGroup:FormGroup
   constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required],
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', Validators.required],
+      birthday: ['', Validators.required],
+      phone: ['', Validators.required],
     });
     this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required],
+      addressTitle: ['', Validators.required],
+      city: ['', Validators.required],
+      district: ['', Validators.required],
+      street: ['', Validators.required],
+      apartment: ['', Validators.required],
+      address: ['', Validators.required],
     });
+    this.thirdFormGroup = this._formBuilder.group({
+      services: ['',Validators.requiredTrue],
+      terms: ['',Validators.requiredTrue],
+      privacy: ['',Validators.requiredTrue],
+    });
+    this.fourtFormGroup = this._formBuilder.group({
+    });
+
   }
 }
