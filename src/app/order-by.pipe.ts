a9f3330de45f9575607faefe38a7f2ed8c8b7e32
Yeyilmaz99 <yeyilmaz34@gmail.com>
2021-12-12 01:16:59 +0300

    second-commit

--- src/app/order-by.pipe.ts
+++ src/app/order-by.pipe.ts
@@ -1,13 +1,18 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
+import { Injectable, Pipe, PipeTransform } from '@angular/core';
+import { orderBy } from 'lodash';
+import { Product } from './products/products.component';
+import { ProductData } from './products/products.datasource';
+@Injectable({
+  providedIn: 'root'
+})
 @Pipe({
   name: 'orderBy'
 })
 export class OrderByPipe implements PipeTransform {
-
-  transform(value: [],):any {
-    if (!value) return null;
-    return value.sort()
-  }
-
-}
+  products = ProductData
+  transform(array: Product[] , sortBy: any, order?: any): any[] {
+    const sortOrder = order ? order : ''; // setting default ascending order
+   
+     return orderBy(array, [sortBy], [sortOrder]);
+     }
+   }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,7 +1,7 @@
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
-
+import { OrderByPipe } from '../order-by.pipe';
 
 
 @Component({
@@ -18,7 +18,9 @@ export class ProductsComponent implements OnInit {
   pageSizeOptions: number[] = [5, 10, 25, 100];
   pageEvent: PageEvent;
 
-  constructor(private productService:ProductService) {}
+  constructor(private productService:ProductService, 
+    private order:OrderByPipe
+    ) {}
   ngOnInit(): void {
     this.getProducts();
   }
@@ -41,6 +43,18 @@ export class ProductsComponent implements OnInit {
   onSelect(product:Product):void{
     this.selectedProduct = product
     console.log("productid"+product.id)
+  }
+  sortHigh(){
+    let newarr = this.productSlice.sort((a,b) =>a.price - b.price);
+    newarr= this.productSlice 
+  }
+  sortLow(){
+    let newarr1= this.productSlice.sort((a,b)=> b.price - a.price);
+    newarr1 = this.productSlice 
+  }
+  search(){
+    this.productSlice= this.order.transform(this.productSlice,'price','desc')
+    console.log(this.productSlice)
   }
 
 
