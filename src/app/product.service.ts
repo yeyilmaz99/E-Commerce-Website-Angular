403c8dc6912dd1f57c1928c3de3e9e3b4965037f
Yunus Emre Yılmaz <yeyilmaz34@gmail.com>
2022-06-18 15:12:44 +0300

    fix

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,23 +2,23 @@ import { Product } from './products/products.component';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { ListResponseModel } from './models/listResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  apiUrl = 'https://localhost:44374/api/';
   cartProducts:Product[]= [];
   data:Product[]=[];
   private apiProductsUrl = 'api/products'
   constructor(private http:HttpClient) { }
 
 
-  public getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(`http://api.myshopapp.tk/v1/products`);
-      // return this.http.get<Product[]>(this.apiProductsUrl);
-
+  getProducts(): Observable<ListResponseModel<Product>> {
+    let newPath = this.apiUrl + 'products/getall';
+    return this.http.get<ListResponseModel<Product>>(newPath);
   }
-
   getProduct(id:number): Observable<Product>{
     return this.http.get<Product>(`http://api.myshopapp.tk/v1/products/`+'/'+id);
     // return this.http.get<Product>(this.apiProductsUrl+'/'+id);
